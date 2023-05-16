85e575943e4be348052306e4aeeb90171352593a
Adem Sinanovic <ademsinanovic235@gmail.com>
2023-05-16 15:35:31 +0200

    Initial commit. Created a responsive header with a fully functioning nav.

---
+++ src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Logo.css";
+
+const Logo = () => {
+  return (
+    <div className="logo-container">
+      <h1 className="logo">BlogIT</h1>
+    </div>
+  );
+};
+
+export default Logo;
